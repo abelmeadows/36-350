61180d25d334c41221a79c46ed7bd20625b0ceaa
Christine <jumpchristine@gmail.com>
2018-04-17 14:27:52 -0400

    Add model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,3 +3,11 @@ generate_data = function(n, p) {
   covariates = matrix(responses, n, p)
   return(list(covariates, responses))
 }
+
+model_select = function(covariates, responses, cutoff) {
+  lin.reg = lm(responses ~ covariates)
+  ret.cov = subset(covariates, summary(lin.reg)$coefficients[, 4] <= cutoff)
+  if (length(ret.cov) == 0) return(vector(length = 0))
+  lin.reg2 = lm(responses ~ ret.cov)
+  return(summary(lin.reg2)$coefficients[, 4])
+}
