36a56362f2acf128809fc95cd8d696f96608e830
Christine <jumpchristine@gmail.com>
2018-04-18 11:09:41 -0400

    Add run_simulation function, Modify previous functions

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,13 +1,33 @@
 generate_data = function(n, p) {
+  covariates = matrix(rnorm(n*p, 0, 1), n, p)
   responses = rnorm(n, 0, 1)
-  covariates = matrix(responses, n, p)
-  return(list(covariates, responses))
+  return(list(covariates = covariates, responses = responses))
 }
 
 model_select = function(covariates, responses, cutoff) {
   lin.reg = lm(responses ~ covariates)
-  ret.cov = subset(covariates, summary(lin.reg)$coefficients[, 4] <= cutoff)
-  if (length(ret.cov) == 0) return(vector(length = 0))
-  lin.reg2 = lm(responses ~ ret.cov)
-  return(summary(lin.reg2)$coefficients[, 4])
+  all.p.vals = (summary(lin.reg)$coefficients)[-1, 4]
+  ret.idx = which(all.p.vals <= cutoff)
+  if (length(ret.idx) == 0) return(c())
+  lin.reg2 = lm(responses ~ covariates[, ret.idx])
+  new.p.vals = (summary(lin.reg2)$coefficients)[-1, 4]
+  return(new.p.vals)
+}
+
+run_simulation = function(n_trials, n, p, cutoff) {
+  p.values = c()
+  for (i in 1:n_trials) {
+    dat = generate_data(n, p)
+    cov = dat$covariates
+    res = dat$responses
+    p.values = c(p.values, model_select(cov, res, cutoff))
+  }
+  hist(p.values)
+}
+
+par(mfrow=c(3, 3), mar=c(2, 2, 2, 2))
+for (i in c(100, 1000, 10000)) {
+  for (j in c(10, 20, 50)) {
+    run_simulation(100, i, j, cutoff = 0.05)
+  }
 }
